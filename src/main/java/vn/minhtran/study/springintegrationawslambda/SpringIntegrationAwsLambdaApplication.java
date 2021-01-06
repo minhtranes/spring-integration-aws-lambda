1ad807438e1a9c4dcfb7401c6140b8be431b2ef5
Minh Tran <thminh_1@spsvietnam.vn>
2021-01-06 11:45:40 +0700

    Initial commit

---
+++ src/main/java/vn/minhtran/study/springintegrationawslambda/SpringIntegrationAwsLambdaApplication.java
@@ -0,0 +1,13 @@
+package vn.minhtran.study.springintegrationawslambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationAwsLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationAwsLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/minhtran/study/springintegrationawslambda/SpringIntegrationAwsLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.minhtran.study.springintegrationawslambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationAwsLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
